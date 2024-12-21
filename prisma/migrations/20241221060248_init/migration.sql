1b1d6b902c2b57ec0a68f813e53a59edc633b8e1
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-21 10:06:29 +0000

    update

---
+++ prisma/migrations/20241221060248_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "Room" TEXT NOT NULL,
+    "Day" TEXT NOT NULL,
+    "StartTime" TEXT NOT NULL,
+    "EndTime" TEXT NOT NULL
+);
