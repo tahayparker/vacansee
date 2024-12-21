ab50ad66be06df4088582f6964d1fb1616875f97
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-21 22:58:51 +0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,14 @@ const Footer = () => {
       <div className="flex items-center justify-between h-full px-4">
         <div className="flex items-center gap-6">
           <Link href="/about" legacyBehavior>
-            <a className="text-sm hover:underline">About</a>
+            <a className="text-sm hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
+              About
+            </a>
           </Link>
           <Link href="/terms" legacyBehavior>
-            <a className="text-sm hover:underline">Terms</a>
+            <a className="text-sm hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
+              Terms
+            </a>
           </Link>
         </div>
         <div className="flex items-center">
