c526e9b79c02d2b29e63444872d3304a802cae6e
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-21 22:58:51 +0400

    update

--- src/pages/CurrentlyAvailable.js
+++ src/pages/CurrentlyAvailable.js
@@ -6,8 +6,10 @@ const CurrentlyAvailable = () => {
   return (
     <div className="min-h-screen flex flex-col page-transition">
       <Header />
-      <main className="flex-grow w-full max-w-6xl mx-auto px-4 pb-16">
-        <RoomList />
+      <main className="flex-grow w-full max-w-6xl mx-auto px-0 sm:px-4 pb-16 pt-24">
+        <div className="w-full flex items-center justify-center">
+          <RoomList />
+        </div>
       </main>
       <Footer className="mt-auto" />
     </div>
