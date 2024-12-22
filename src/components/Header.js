8d6bacf2c8c78b6d64467c1a88f8846d3b3e0e8e
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-22 15:15:37 +0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,8 +31,8 @@ const Header = ({ hideLogoOnHome }) => {
         onClick={toggleMenu}
       />
 
-      <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-black/30 border-b border-[#482f1f]">
-        <div className="flex items-center justify-between h-[70] px-4">
+      <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-black/30 border-b border-[#482f1f] h-[70px]">
+        <div className="flex items-center justify-between h-[70px] px-4">
           <div className="flex items-center gap-4">
             {!hideLogoOnHome && (
               <Link href="/" legacyBehavior>
