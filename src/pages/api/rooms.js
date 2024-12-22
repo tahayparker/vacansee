1fb32f563ed5b93dd1013da74a7a78f49fe201af
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-22 11:09:16 +0400

    update

--- src/pages/api/rooms.js
+++ src/pages/api/rooms.js
@@ -1,37 +1,33 @@
-import { open } from 'sqlite';
-import sqlite3 from 'sqlite3';
+import prisma from '../../lib/db';
 
 const getFreeRooms = async (req, res) => {
-  const db = await open({
-    filename: 'classes.db',
-    driver: sqlite3.Database
-  });
+  try {
+    const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
 
-  const freeRoomsQuery = `
-SELECT DISTINCT Room
-FROM Classes
-WHERE Room NOT IN (
-    SELECT Room
-    FROM Classes
-    WHERE 
-        Day = CASE strftime('%w', 'now')
-                  WHEN '0' THEN 'Sunday'
-                  WHEN '1' THEN 'Monday'
-                  WHEN '2' THEN 'Tuesday'
-                  WHEN '3' THEN 'Wednesday'
-                  WHEN '4' THEN 'Thursday'
-                  WHEN '5' THEN 'Friday'
-                  WHEN '6' THEN 'Saturday'
-              END
-        AND time('now', '+04:00') BETWEEN StartTime AND EndTime
-)
-ORDER BY Room ASC;
-  `;
-  const freeRooms = await db.all(freeRoomsQuery);
+    const freeRooms = await prisma.class.findMany({
+      where: {
+        NOT: {
+          AND: [
+            { Day: currentDay },
+            { StartTime: { lte: currentTime } },
+            { EndTime: { gte: currentTime } }
+          ]
+        }
+      },
+      select: {
+        Room: true
+      },
+      distinct: ['Room'],
+      orderBy: {
+        Room: 'asc'
+      }
+    });
 
-  await db.close();
-
-  res.status(200).json(freeRooms.map(room => room.Room));
+    res.status(200).json(freeRooms.map(room => room.Room));
+  } catch {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 export default getFreeRooms;
