af6364ff836d87583d5ba461137436544957bb91
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-22 11:09:14 +0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,23 +2,23 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="w-full h-16 bg-background text-foreground backdrop-blur-md bg-opacity-60">
+    <footer className="w-full h-16 backdrop-blur-md bg-black/30 border-t border-[#482f1f]">
       <div className="flex items-center justify-between h-full px-4">
         <div className="flex items-center gap-6">
           <Link href="/about" legacyBehavior>
-            <a className="text-sm hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
+            <a className="text-sm text-gray-300 hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
               About
             </a>
           </Link>
           <Link href="/terms" legacyBehavior>
-            <a className="text-sm hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
+            <a className="text-sm text-gray-300 hover:underline hover:text-[#006D5B] transition-all duration-200 ease-in-out">
               Terms
             </a>
           </Link>
         </div>
         <div className="flex items-center">
-          <p className="text-sm">
-            Made with <span className="text-black">🖤</span> by TP
+          <p className="text-sm text-gray-300">
+            Made with <span className="text-[#006D5B]">🖤</span> by TP
           </p>
         </div>
       </div>
