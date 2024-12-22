4b8e8f7ba94f5a5dad0cf186fb9f7faf08ca0c16
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-22 11:09:15 +0400

    update

--- src/pages/api/check-availability.js
+++ src/pages/api/check-availability.js
@@ -1,29 +1,27 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
+import prisma from '../../lib/db';
+
 export default async function handler(req, res) {
   const { room, day, startTime, endTime } = req.query;
-  // Open a connection to the database
-  const db = await open({
-    filename: 'classes.db',
-    driver: sqlite3.Database,
-  });
+
   try {
     // Query to check room availability based on the given time range
-    const query = `
-      SELECT * FROM classes
-      WHERE Room = ?
-      AND Day = ?
-      AND NOT (EndTime <= ? OR StartTime >= ?)
-    `;
-    const result = await db.all(query, [room, day, startTime, endTime]);
+    const result = await prisma.class.findMany({
+      where: {
+        Room: room,
+        Day: day,
+        NOT: {
+          OR: [
+            { EndTime: { lte: startTime } },
+            { StartTime: { gte: endTime } }
+          ]
+        }
+      }
+    });
+
     const isAvailable = result.length === 0;
-    
     res.status(200).json({ available: isAvailable });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    // Close the database connection
-    await db.close();
   }
 }
