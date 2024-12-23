69cdabd6fd8970091f417f14c22a3e5e81672e80
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-23 07:33:39 +0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx', ...config.resolve.extensions];
     return config;
