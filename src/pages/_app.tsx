1f7b82ebe80dd97faa7248e0a2e2d59b14c166b9
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-12-24 20:32:44 +0400

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,11 @@
-import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import type { Session } from 'next-auth';
 import '../styles/globals.css';
-
-type CustomAppProps = AppProps<{
-  session: Session | null;
-}>;
 
 export default function App({ 
   Component, 
-  pageProps: { session, ...pageProps }
-}: CustomAppProps) {
+  pageProps 
+}: AppProps) {
   return (
-    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
   );
 }
