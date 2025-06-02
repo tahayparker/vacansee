e7984a4747d437fde7b3e72b47870813f817c590
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-06-02 18:17:48 +0400

    update

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,185 +4,185 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       _prisma_migrations: {
         Row: {
-          applied_steps_count: number
-          checksum: string
-          finished_at: string | null
-          id: string
-          logs: string | null
-          migration_name: string
-          rolled_back_at: string | null
-          started_at: string
-        }
+          applied_steps_count: number;
+          checksum: string;
+          finished_at: string | null;
+          id: string;
+          logs: string | null;
+          migration_name: string;
+          rolled_back_at: string | null;
+          started_at: string;
+        };
         Insert: {
-          applied_steps_count?: number
-          checksum: string
-          finished_at?: string | null
-          id: string
-          logs?: string | null
-          migration_name: string
-          rolled_back_at?: string | null
-          started_at?: string
-        }
+          applied_steps_count?: number;
+          checksum: string;
+          finished_at?: string | null;
+          id: string;
+          logs?: string | null;
+          migration_name: string;
+          rolled_back_at?: string | null;
+          started_at?: string;
+        };
         Update: {
-          applied_steps_count?: number
-          checksum?: string
-          finished_at?: string | null
-          id?: string
-          logs?: string | null
-          migration_name?: string
-          rolled_back_at?: string | null
-          started_at?: string
-        }
-        Relationships: []
-      }
+          applied_steps_count?: number;
+          checksum?: string;
+          finished_at?: string | null;
+          id?: string;
+          logs?: string | null;
+          migration_name?: string;
+          rolled_back_at?: string | null;
+          started_at?: string;
+        };
+        Relationships: [];
+      };
       allowed_emails: {
         Row: {
-          created_at: string
-          email: string
-        }
+          created_at: string;
+          email: string;
+        };
         Insert: {
-          created_at?: string
-          email: string
-        }
+          created_at?: string;
+          email: string;
+        };
         Update: {
-          created_at?: string
-          email?: string
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          email?: string;
+        };
+        Relationships: [];
+      };
       Rooms: {
         Row: {
-          Capacity: number | null
-          id: number
-          Name: string
-          ShortCode: string
-        }
+          Capacity: number | null;
+          id: number;
+          Name: string;
+          ShortCode: string;
+        };
         Insert: {
-          Capacity?: number | null
-          id?: number
-          Name: string
-          ShortCode: string
-        }
+          Capacity?: number | null;
+          id?: number;
+          Name: string;
+          ShortCode: string;
+        };
         Update: {
-          Capacity?: number | null
-          id?: number
-          Name?: string
-          ShortCode?: string
-        }
-        Relationships: []
-      }
+          Capacity?: number | null;
+          id?: number;
+          Name?: string;
+          ShortCode?: string;
+        };
+        Relationships: [];
+      };
       SignInLog: {
         Row: {
-          authProvider: Database["public"]["Enums"]["AuthProvider"]
-          email: string
-          id: string
-          name: string | null
-          success: boolean
-          timestamp: string
-        }
+          authProvider: Database["public"]["Enums"]["AuthProvider"];
+          email: string;
+          id: string;
+          name: string | null;
+          success: boolean;
+          timestamp: string;
+        };
         Insert: {
-          authProvider: Database["public"]["Enums"]["AuthProvider"]
-          email: string
-          id: string
-          name?: string | null
-          success: boolean
-          timestamp: string
-        }
+          authProvider: Database["public"]["Enums"]["AuthProvider"];
+          email: string;
+          id: string;
+          name?: string | null;
+          success: boolean;
+          timestamp: string;
+        };
         Update: {
-          authProvider?: Database["public"]["Enums"]["AuthProvider"]
-          email?: string
-          id?: string
-          name?: string | null
-          success?: boolean
-          timestamp?: string
-        }
-        Relationships: []
-      }
+          authProvider?: Database["public"]["Enums"]["AuthProvider"];
+          email?: string;
+          id?: string;
+          name?: string | null;
+          success?: boolean;
+          timestamp?: string;
+        };
+        Relationships: [];
+      };
       Teacher: {
         Row: {
-          Email: string
-          id: number
-          Name: string
-          Phone: string
-        }
+          Email: string;
+          id: number;
+          Name: string;
+          Phone: string;
+        };
         Insert: {
-          Email: string
-          id?: number
-          Name: string
-          Phone: string
-        }
+          Email: string;
+          id?: number;
+          Name: string;
+          Phone: string;
+        };
         Update: {
-          Email?: string
-          id?: number
-          Name?: string
-          Phone?: string
-        }
-        Relationships: []
-      }
+          Email?: string;
+          id?: number;
+          Name?: string;
+          Phone?: string;
+        };
+        Relationships: [];
+      };
       Timings: {
         Row: {
-          Class: string
-          Day: string
-          EndTime: string
-          id: number
-          Room: string
-          StartTime: string
-          SubCode: string
-          Teacher: string
-        }
+          Class: string;
+          Day: string;
+          EndTime: string;
+          id: number;
+          Room: string;
+          StartTime: string;
+          SubCode: string;
+          Teacher: string;
+        };
         Insert: {
-          Class: string
-          Day: string
-          EndTime: string
-          id?: number
-          Room: string
-          StartTime: string
-          SubCode: string
-          Teacher: string
-        }
+          Class: string;
+          Day: string;
+          EndTime: string;
+          id?: number;
+          Room: string;
+          StartTime: string;
+          SubCode: string;
+          Teacher: string;
+        };
         Update: {
-          Class?: string
-          Day?: string
-          EndTime?: string
-          id?: number
-          Room?: string
-          StartTime?: string
-          SubCode?: string
-          Teacher?: string
-        }
-        Relationships: []
-      }
-    }
+          Class?: string;
+          Day?: string;
+          EndTime?: string;
+          id?: number;
+          Room?: string;
+          StartTime?: string;
+          SubCode?: string;
+          Teacher?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      AuthProvider: "GOOGLE" | "GITHUB" | "UNDETERMINED"
-    }
+      AuthProvider: "GOOGLE" | "GITHUB" | "UNDETERMINED";
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type DefaultSchema = Database[Extract<keyof Database, "public">]
+type DefaultSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
@@ -190,7 +190,7 @@ export type Tables<
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -198,64 +198,64 @@ export type Tables<
         DefaultSchema["Views"])
     ? (DefaultSchema["Tables"] &
         DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
     ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
@@ -263,14 +263,14 @@ export type Enums<
   ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
     ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -278,7 +278,7 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
     ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+    : never;
 
 export const Constants = {
   public: {
@@ -286,4 +286,4 @@ export const Constants = {
       AuthProvider: ["GOOGLE", "GITHUB", "UNDETERMINED"],
     },
   },
-} as const
+} as const;
