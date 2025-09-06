d8fa64d9797b14f4020ff60625c440c767b412a2
Taha Yaseen Parker <98612931+tahayparker@users.noreply.github.com>
2025-09-06 11:25:33 +1000

    update

---
+++ src/pages/500.tsx
@@ -0,0 +1,26 @@
+// pages/500.tsx
+import Link from "next/link";
+import React from "react";
+import { BadgeAlert } from "lucide-react";
+
+export default function Custom500() {
+  // Return only the content container. Centering handled by <main> in _app.tsx
+  return (
+    <div className="space-y-4 text-center">
+      {" "}
+      <BadgeAlert className="mx-auto h-16 w-16 text-red-500" />
+      <h1 className="text-4xl sm:text-5xl font-bold text-white/90">
+        Error 500
+      </h1>
+      <p className="text-lg text-white/70">
+        Oops! Something went wrong on our end.
+      </p>
+      <Link
+        href="/"
+        className="inline-block mt-6 px-6 py-2 rounded-full bg-purple-600 text-white hover:bg-purple-700 transition-colors"
+      >
+        Go back home
+      </Link>
+    </div>
+  );
+}
