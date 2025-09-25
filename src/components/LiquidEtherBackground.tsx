2f44fbd9153f35ef1f138919e82f348bb6b818ca
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-09-25 23:18:57 +1000

    update [skip deploy]

---
+++ src/components/LiquidEtherBackground.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import LiquidEther from "./LiquidEther";
+
+const LiquidEtherBackground: React.FC<{ children?: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: -1,
+        overflow: "hidden",
+        pointerEvents: "none",
+      }}
+    >
+      <LiquidEther
+        colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+        mouseForce={20}
+        cursorSize={100}
+        isViscous={false}
+        viscous={30}
+        iterationsViscous={32}
+        iterationsPoisson={32}
+        resolution={0.5}
+        isBounce={false}
+        autoDemo={true}
+        autoSpeed={0.5}
+        autoIntensity={2.2}
+        takeoverDuration={0.25}
+        autoResumeDelay={3000}
+        autoRampDuration={0.6}
+        style={{
+          width: "100%",
+          height: "100%",
+          pointerEvents: "auto"
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default LiquidEtherBackground;
