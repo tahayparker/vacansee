a4248efee090ef4d23c199d4fc04b9dd08048116
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-10-21 13:08:29 +1100

    update

--- src/components/PlasmaBackground.tsx
+++ src/components/PlasmaBackground.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import Plasma from './Plasma';
+import React from "react";
+import Plasma from "./Plasma";
 
 const PlasmaBackground: React.FC = () => {
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0, pointerEvents: 'none' }}>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: 0,
+        pointerEvents: "none",
+      }}
+    >
       <Plasma
         color="#8b5cf6"
         speed={2}
