e27c5691809d12146285de07f1842783f62200a1
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2025-10-29 14:57:55 +1100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
         >
           <Link
             className="group relative inline-flex items-center justify-center rounded-full border border-solid border-transparent transition-colors bg-purple-500 text-white gap-2 shadow-lg hover:bg-purple-500 hover:shadow-purple-500/30 font-medium text-sm sm:text-base h-11 sm:h-12 px-6 sm:px-8 w-full sm:w-auto focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-background"
-            href="/check"
+            href="/available-now"
           >
             <DoorOpen className="h-5 w-5 transition-transform duration-200 group-hover:translate-y-[-2px]" />
             Available Now
